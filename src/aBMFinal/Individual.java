e3b81dcae9c2ec526ab7c2ccc8d71f7683426f06
Edgar Avalos <Edgarav@Edgarav-Air.local>
2018-05-03 15:03:18 -0500

    First commit

---
+++ src/aBMFinal/Individual.java
@@ -0,0 +1,15 @@
+package aBMFinal;
+
+import java.util.ArrayList;
+
+// Instance Variables
+public class Individual {
+	static float propInheritance;
+	
+	double wealth;
+	double talent;
+	ArrayList<Individual> socialNetwork;
+	
+}
+
+// Methods
